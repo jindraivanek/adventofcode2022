70141e7d80f25e3a5d0184b00c9d787e2a612a9c
jindraivanek <jindra.ivanek@gmail.com>
2022-12-13 09:04:37 +0100

    day 13 wip

--- day13.fsx
+++ day13.fsx
@@ -13,12 +13,19 @@ type Packet =
 
 let packetParser() = 
     let digit = satisfy (fun ch -> System.Char.IsDigit ch)
-    let number = digit |>> (int >> Number)
+    let number = digit |>> (string >> int >> Number)
     let packet, packetRef = createParserForwardedToRef<Packet, unit>()
     let list = pchar '[' >>. sepBy packet (pchar ',') .>> pchar ']' |>> List
     packetRef := choice [ number; list ]
     packet
+let parse str =
+    match run (packetParser()) str with
+    | Success(result, _, _)   -> 
+        printfn "Success: %A" result
+        result
+    | Failure(errorMsg, _, _) -> failwithf "Failure: %s" errorMsg
 
+parse "[1,[2,[]],3]"
 
 let part1 = 0
 printfn $"PART1: {part1}"
