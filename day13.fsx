c3547a2a3054eba526708947b471719d39a64986
Jindřich Ivánek <jindra.ivanek@gmail.com>
2022-12-13 17:29:14 +0100

    day 13 wip

--- day13.fsx
+++ day13.fsx
@@ -3,9 +3,6 @@ open FParsec
 
 let lines = System.IO.File.ReadAllLines("day13.test")
 
-let packetPairs =
-    lines
-    |> Array.chunkBySize 3 |> Array.map (fun xs -> xs[0], xs[1])
 
 type Packet =
     | Number of int
@@ -13,7 +10,7 @@ type Packet =
 
 let packetParser() = 
     let digit = satisfy (fun ch -> System.Char.IsDigit ch)
-    let number = digit |>> (string >> int >> Number)
+    let number = many1 digit |>> (List.map string >> String.concat "" >> int >> Number)
     let packet, packetRef = createParserForwardedToRef<Packet, unit>()
     let list = pchar '[' >>. sepBy packet (pchar ',') .>> pchar ']' |>> List
     packetRef := choice [ number; list ]
@@ -25,9 +22,37 @@ let parse str =
         result
     | Failure(errorMsg, _, _) -> failwithf "Failure: %s" errorMsg
 
-parse "[1,[2,[]],3]"
+let packetPairs =
+    lines
+    |> Array.chunkBySize 3 |> Array.map (fun xs -> parse xs[0], parse xs[1])
 
-let part1 = 0
+type Ordering =
+    | Right
+    | Undecided
+    | NotRight
+
+let rec packetOrder = function
+    | Number a, Number b when a < b -> Right
+    | Number a, Number b when a > b -> NotRight
+    | Number a, Number b when a = b -> Undecided
+    | List (a :: ta), List (b :: tb) -> 
+        match packetOrder (a, b) with
+        | Right -> Right
+        | NotRight -> NotRight
+        | Undecided -> packetOrder (List ta, List tb)
+    | List [], List (_ :: _) -> Right
+    | List (_ :: _), List [] -> NotRight
+    | List [], List [] -> Undecided
+    | a, Number b -> packetOrder (a, List [Number b])
+    | Number a, b -> packetOrder (List [Number a], b)
+
+let rightPacketPairs = packetPairs |> Seq.indexed |> Seq.filter (fun (_, p) -> packetOrder p = Right) |> Seq.map (fst >> (+) 1) |> Seq.toList
+printfn "%A" rightPacketPairs
+
+let orderedPackets = packetPairs |> Seq.collect (fun (x,y) -> seq{x; y}) |> Seq.sortWith (fun x y -> packetOrder (x,y) |> (function |Right -> -1 |NotRight -> 1 |Undecided->0)) |> Seq.toList
+printfn "%A" orderedPackets
+
+let part1 = rightPacketPairs |> Seq.sum
 printfn $"PART1: {part1}"
 
 let part2 = 0
